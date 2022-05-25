999928862f5ab02173576707c57340dd64796e41
Kenan20222 <104547432+Kenan20222@users.noreply.github.com>
2022-05-25 23:28:22 +0400

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 
 module.exports = ({
 
-token: "5395714639:AAEVcE5YbhacspgYBnGZkwRkkrkG-WG_hjo",
+token: 5119966316:AAHqxJSxgE8fxcP4LB_qNCQu87LuV9g0uVo
 startmesaj: " <b><i>Salam. Bota etiraf etmək istədiyin mesajı yazırsan və kanalda paylaşılır.\n\nEtirafınızı yazdıxdan sonra Anonim(gizli) yoxsa Açıq paylaşım soruşacam ✅\n\nBuyur Bir Etiraf Et 💁🏼‍♀️</i></b>",
 aciqetirafeden: "📣 Açıq Etiraf \n👥İstifadəçi: ",
 aciqyazdigietiraf: "💄Yazdığı Etiraf: ",
